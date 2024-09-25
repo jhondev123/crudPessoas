a9c2e1a5266aebb3ef5169bed78423ac0fdf5a05
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-09-25 01:38:42 -0300

    resolvendo problema de não encontrar variaveis de ambiente

--- index.php
+++ index.php
@@ -4,10 +4,18 @@ use CoffeeCode\Router\Router;
 use Dotenv\Dotenv;
 
 require_once __DIR__ . "/vendor/autoload.php";
+if (file_exists(__DIR__ . "/.env")) {
 
-var_dump(getenv('DB_HOST'));exit();
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}else{
+    $_ENV['APP_URL'] = getenv('APP_URL');
+    $_ENV['DB_HOST'] = getenv('DB_HOST');
+    $_ENV['DB_PORT'] = getenv('DB_PORT');
+    $_ENV['DB_DATABASE'] = getenv('DB_DATABASE');
+    $_ENV['DB_USERNAME'] = getenv('DB_USERNAME');
+    $_ENV['DB_PASSWORD'] = getenv('DB_PASSWORD');
+}
 
 $router = new Router($_ENV['APP_URL']);
 $router->namespace("Jhonattan\CrudPessoas\Http\Controllers");
